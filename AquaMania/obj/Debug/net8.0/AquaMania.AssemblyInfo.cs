d396705c5defb4c3c7c113749beedf64abb035e3
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2024-08-07 17:14:31 -0300

    .

--- AquaMania/obj/Debug/net8.0/AquaMania.AssemblyInfo.cs
+++ AquaMania/obj/Debug/net8.0/AquaMania.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AquaMania")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+656055e4418fa7b8f080834f25a99e0bba78dc2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f9b48936ba07cc5327925c701519f008793a89f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AquaMania")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AquaMania")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
